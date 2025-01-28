7b0ba37a0e4f2cc77a26d4a4b47ff99e3b8cee13
BoutahirYassine <125197661+BoutahirYassine@users.noreply.github.com>
2025-01-28 17:44:40 +0100

    commit

--- conversion.py
+++ conversion.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Charger les fichiers Excel
-argent_file = "DataBI/Prix_cellule.xlsx"
-conversion_file = "DataBI/USD_CNY.xlsx"
+argent_file = "DataBI/Cell_TOPCON_M10.xlsx"
+conversion_file = "DataBI/EUR_CNY.xlsx"
 
 argent_data = pd.read_excel(argent_file)
 conversion_data = pd.read_excel(conversion_file)
